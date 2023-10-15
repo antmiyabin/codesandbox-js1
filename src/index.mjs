9dfe1c58844fc2362755dd7d69d54d23b1f4bf6b
antmiyabin <80366657+antmiyabin@users.noreply.github.com>
2023-10-15 11:27:57 +0000

    Update index.html and index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -31,15 +31,54 @@
 // // const変数は再宣言不可
 // const val3 = "const変数を上書き";
 
-const val4 = {
-  name: "じゃけぇ",
-  age: 28
-};
-val4.name = "jak";
-val4.address = "Hiroshima";
-console.log(val4);
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
 
-const val5 = ["dog", "cat"];
-val5[0] = "rabbit";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "rabbit";
+// val5.push("monkey");
+// console.log(val5);
+
+// /**
+//  * テンプレート文字列
+//  */
+// const name = "miyabin";
+// const age = 45;
+
+// // past
+
+// const msg1 = "My name is " + name + ". I'm " + age + " years old";
+// console.log(msg1);
+
+// const msg2 = `My name is ${name}. I'm ${age} years old.`;
+// console.log(msg2);
+
+/**
+ * past function
+ */
+const func1 = function(str) {
+  return str;
+}
+console.log(func1("func1"));
+
+/**
+ * arrow function
+ */
+const func2 = (str) => {
+  return str;
+}
+console.log(func2("func2"));
+
+/**
+ * arrow function
+ */
+const func3 = (str) => str + "!";
+console.log(func3("func3"));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(2, 4));
